030c61ca7e37f11c0168657708085b0672a46ddb
Ivan-Niki <iFankov@gmail.com>
2023-08-26 20:20:12 +0300

    add2 myFree-js

--- myFree.js
+++ myFree.js
@@ -19,7 +19,24 @@ function reverseString(str) {
 
 reverseString("hello");
 
+//3. Factorialize a Number
+//Return the factorial of the provided integer.
+//If the integer is represented with the letter n, a factorial is the product of all positive integers less than or //equal to n.
+//Factorials are often represented with the shorthand notation n!
+//For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+//Only integers greater than or equal to zero will be supplied to the function.
 
+function factorialize(num) {
+  if (num == 0) {
+    return (1);
+  }
+  for (let i = num-1; i >=1; i--) {
+    num = num * i
+  }
+  return num;
+}
+
+factorialize(5);
 
 
 
