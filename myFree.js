36d3d7aebbc355096bbed07e755dad8626704da4
Ivan-Niki <iFankov@gmail.com>
2023-08-28 16:31:19 +0300

    add Return-Largest-Numbers-in-Arrays task

--- myFree.js
+++ myFree.js
@@ -57,5 +57,18 @@ function findLongestWordLength(str) {
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
 
 
+//5. Return Largest Numbers in Arrays
+//Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array //will contain exactly 4 sub-arrays.
+//Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+function largestOfFour(arr) {
+  let result = [];
+  for (let i=0; i<arr.length; i++) {
+    result.push(Math.max(...arr[i]))
+  }
+  return result;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
 
