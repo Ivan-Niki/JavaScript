29b5c7ef90e8f8b7333172991bacf4134474d6be
Ivan-Niki <iFankov@gmail.com>
2023-08-28 22:18:10 +0300

    add Confirm-the-Ending task

--- myFree.js
+++ myFree.js
@@ -72,3 +72,16 @@ function largestOfFour(arr) {
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
 
+//6. Confirm the Ending
+//Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+//This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of //this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+function confirmEnding(str, target) {
+  return str.slice(str.length - target.length) === target;
+ 
+}
+
+confirmEnding("Bastian", "n");
+
+
