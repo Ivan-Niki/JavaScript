6404140f7f75d504018badd0e637b9600d398dd0
Ivan-Niki <iFankov@gmail.com>
2023-08-29 00:52:17 +0300

    add Truncate-a-String task

--- myFree.js
+++ myFree.js
@@ -101,4 +101,18 @@ function repeatStringNumTimes(str, num) {
 
 repeatStringNumTimes("abc", 3);
 
+//8. Truncate a String
+//Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return //the truncated string with a ... ending.
+
+function truncateString(str, num) {
+  if (str.length <= num) {
+    return str;
+  } else {
+    return (str.slice(0, num) + "...");
+  }
+  
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
 
