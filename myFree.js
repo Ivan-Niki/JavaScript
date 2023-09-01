7b0a5ccdaba9bb1085025ff8aa8fba1f24dde79d
Ivan-Niki <iFankov@gmail.com>
2023-09-01 21:41:08 +0300

    add falsy-bouncer task

--- myFree.js
+++ myFree.js
@@ -176,6 +176,24 @@ function frankenSplice(arr1, arr2, n) {
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
 
+// 13. Falsy Bouncer
+// Remove all falsy values from an array. Return a new array; do not mutate the original array.
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+// Hint: Try converting each value to a Boolean.
+// bouncer([7, "ate", "", false, 9]) should return [7, "ate", 9]
+
+function bouncer(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+bouncer([7, "ate", "", false, 9]);
+
 
 
 
