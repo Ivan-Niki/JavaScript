ed3b11f71747373386c6bbbfd18d5728b633049b
Ivan-Niki <iFankov@gmail.com>
2024-02-02 23:35:37 +0300

    add method checkBalance to object task

--- stepik-tasks.js
+++ stepik-tasks.js
@@ -785,3 +785,14 @@ let obj = {
 };
 obj.myMethod(num1, num2);
 
+/* 53. Перед вами программа, которая должна запрашивать баланс банковского счета у пользователя и выводить
+сообщение о балансе в заявленном формате. Вставьте вместо многоточия объявление метода для объекта. */
+const balance = Number(prompt())        
+let bankAccount = {
+	balance,
+	checkBalance() {
+      console.log("Ваш текущий баланс:", balance, "долларов");
+	}
+};
+bankAccount.checkBalance();
+
