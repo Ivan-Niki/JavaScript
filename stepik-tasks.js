730d013588d06b9c8afcd4967f6413770719146d
Ivan-Niki <iFankov@gmail.com>
2024-02-21 23:06:36 +0300

    add for-of loop task

--- stepik-tasks.js
+++ stepik-tasks.js
@@ -1071,4 +1071,21 @@ for (let i = 0; i < numElements; i++) {
 
 console.log(userArray);
 
+/* 77. Перед вами программа, которая сначала запрашивает количество элементов массива, а затем элементы массива 
+по одному соответствующее количество раз. 
+Программа добавляет каждый элемент, введенный пользователем, в конец массива. 
+Далее, с помощью цикла for...of в массив lengths поочерёдно добавляются длина каждого элемента массива userArray.  
+Заполните тела циклов так, чтобы программа работала. */
+const numElements = Number(prompt("Введите количество элементов в массиве:"));
+const userArray = [];
+const lengths = [];
 
+for (let i = 0; i < numElements; i++) {
+    userArray.push(prompt(i));
+}
+
+for (const word of userArray) {
+    lengths.push(word.length);
+}
+
+console.log(lengths);
